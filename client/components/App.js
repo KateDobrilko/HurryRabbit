b28e092a2a8b4e79c5a7ba00f0f65f5728886759
KateDobrilko <katedob1595@gmail.com>
2017-03-27 16:30:49 +0300

    Added flash messages.

--- client/components/App.js
+++ client/components/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import NavigationBar from './NavigationBar/NavigationBar'
-
+import FlashMessagesList from './Flash/FlashMessagesList';
 
 class App extends Component {
     render() {
         return (
+            <div>
+                <NavigationBar />
+                <FlashMessagesList />
+                {this.props.children}
+            </div>
 
-            <NavigationBar />
 
         )
     }
